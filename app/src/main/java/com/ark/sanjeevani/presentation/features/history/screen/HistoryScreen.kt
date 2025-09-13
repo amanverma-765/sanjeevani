2058f636a64f7cd7a17fd0b7a2209b15bd91ae3b
Aman Verma <akverma4aman@gmail.com>
2025-09-13 17:46:40 +0530

    feat: Add NoRecentHistory component to display message when no history is available
    This commit introduces UI enhancements to the `HistoryScreen` and `ProfileScreen`.
    
    Key changes:
    - **HistoryScreen:**
        - Modified `HistoryScreen.kt` to display a `NoRecentHistory` component when there is no recent history. This component fills the entire screen.
        - Added `NoRecentHistory.kt` which shows an icon, a title "No recent history", and a message "Create a new appointment to see it here."
    - **ProfileScreen:**
        - Updated `ProfileScreen.kt` to include a `Scaffold` with a `CenterAlignedTopAppBar`.
        - The top app bar displays the title "Your Profile".
        - The main content area of the screen currently displays the text "Profile Screen" centered.

--- app/src/main/java/com/ark/sanjeevani/presentation/features/history/screen/HistoryScreen.kt
+++ app/src/main/java/com/ark/sanjeevani/presentation/features/history/screen/HistoryScreen.kt
@@ -1,40 +1,15 @@
 package com.ark.sanjeevani.presentation.features.history.screen
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Notifications
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
-import com.ark.sanjeevani.presentation.features.home.components.HomeTopBar
-import com.ark.sanjeevani.utils.DefaultImageLoader.imageLoader
+import com.ark.sanjeevani.presentation.features.history.components.NoRecentHistory
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,6 +23,10 @@ fun HistoryScreen(
         contentPadding = PaddingValues(16.dp),
         modifier = modifier.fillMaxSize()
     ) {
-
+        item {
+            NoRecentHistory(
+                modifier = Modifier.fillParentMaxSize()
+            )
+        }
     }
 }
